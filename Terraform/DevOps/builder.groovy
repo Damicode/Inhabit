557b9392f15579ee4b8c3fd0e2e93dcfd2b26565
Damicode <damierraymond@yahoo.fr>
2024-05-19 22:56:35 -0700

    Update builder.groovy

--- Terraform/DevOps/builder.groovy
+++ Terraform/DevOps/builder.groovy
@@ -1,9 +1,9 @@
 pipeline {
     agent any
-    tools { 
-        maven 'apache-maven-3.8.8' 
-        jdk 'openjdk 17.0.11' 
-    }
+    // tools { 
+    //     // maven 'apache-maven-3.8.8' 
+    //     // jdk 'openjdk 17.0.11' 
+    // }
     stages {
         stage ('Initialize') {
             steps {
